d1fe2bd5bdfb51d26ef44d54c66e7559e9989b8f
Simon Kilroy <simonkilroy1@gmail.com>
2020-03-26 16:51:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // add solution here
+var array = []
+
+function theBeatlesPlay(musicians, instruments) {
+for (let i = 0; i < musicians.length; i += 1) {
+  array.push(`${musicians[i]} plays ${instruments[i]}`);
+}
+return array
+}
+
+// var exclamationFacts = []
+//
+// function johnLennonFacts(facts) {
+// for (let i = 0; i < facts.length; i += 1) {
+//   exclamationFacts.push(`${facts[i]}!!!`);
+// }
+// return exclamationFacts
+// }
+
+function johnLennonFacts(facts) {
+  for (let i = 0; i < facts.length; i += 1) {
+    var fact = facts[i];
+    facts[i] = `${fact}!!!`;
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number) {
+  do {
+    var i = number;
+    var array = [];
+    array.push("I love the Beatles!")
+  } while (i < 15) {
+    array.push("I love the Beatles!")
+    i += 1
+  }
+  return array
+}
